3816eb72b9dde7ffd81793e3ac5da5ac73bf9a13
Guille DF <gdefermin@gmail.com>
2016-03-10 11:38:19 +0100

    Updated linking script

--- linkfiles.sh
+++ linkfiles.sh
@@ -3,5 +3,7 @@
 for file in $(ls); do
     [ "$1" = "--safe" ] && [ -e ~/.$file ] &&
 	echo "Warning: ~/.$file exists, backed up to ~/.${file}~"
+    [ "$file" = "LICENSE" ] || [ "$file" = "linkfiles.sh" ] &&
+	continue
     ln -sb dotfiles/$file ~/.$file
 done
